11eb2138feb658b22f752c3146e23a27dd164fde
Aaron <44809964+aaronjones1@users.noreply.github.com>
2026-02-24 17:26:20 -0500

    Use Fira Code Google Font throughout app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Fira_Code } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
 import { SessionProvider } from "next-auth/react"
 
-const inter = Inter({ subsets: ["latin"] })
+const firaCode = Fira_Code({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Beezwax - Business Intelligence",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, "min-h-screen bg-background antialiased")}>
+      <body className={cn(firaCode.className, "min-h-screen bg-background antialiased")}>
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
